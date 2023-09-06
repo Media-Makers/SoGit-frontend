c956aedbd009282b59d89eaabea5314691e28733
Andrea Malone <andreamurry63@gmail.com>
2023-09-05 20:21:54 -0500

    updateappjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Icon } from "@iconify/react";
+//import { Icon } from "@iconify/react";
 import { Button, Modal, Form, Image } from "react-bootstrap";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-
-
-export default App
+export default App;
