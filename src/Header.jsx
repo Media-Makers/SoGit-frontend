8ae5ba16da76777dc2d9b736e9cbe8f026ebf2f2
Ajamu Page <kil91402@gmail.com>
2023-09-06 17:51:26 -0500

    import css

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import logo from './images/SoGit.png';
+import logo from "./images/SoGit.png";
 class Header extends Component {
     render() {
       return (
