a49181c2c5714d3bae9e56b21b2f8dce622a6c0f
Ajamu Page <kil91402@gmail.com>
2023-09-05 20:43:47 -0500

    added header

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from 
+class Header extends Component {
+    render() {
+      return (
+        <header>
+          <Image src={logo} alt="SoGit Logo" />
+          
+        </header>
+      );
+    }
+  }
+  
