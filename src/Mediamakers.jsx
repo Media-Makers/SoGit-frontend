2db73b9dc53236cd05be34bf6e3037ac7fd67821
Andrea Malone <andreamurry63@gmail.com>
2023-09-06 20:47:33 -0500

    formcreated

---
+++ src/Mediamakers.jsx
@@ -0,0 +1,28 @@
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+
+class HorizontalExample extends Component {
+  render() {
+    return (
+      <Form>
+        <Row>
+          <Col>
+            <Form.Control placeholder="Username" />
+          </Col>
+          <Col>
+            <Form.Control placeholder="Full Name" />
+          </Col>
+          <Col>
+            <Form.Control placeholder="Password" />
+          </Col>
+          <Col>
+            <Form.Control placeholder="Re-enter Password" />
+          </Col>
+        </Row>
+      </Form>
+    );
+  }
+}
+
+export default HorizontalExample;
