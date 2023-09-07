e7511e651c597d731fea3539fa79df399377c6e1
Ajamu Page <kil91402@gmail.com>
2023-09-07 18:21:29 -0500

    added newssearch.jsx

--- App.jsx
+++ App.jsx
@@ -1,30 +1,37 @@
 import React, { Component } from "react";
-//import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-//import { Icon } from "@iconify/react";
-//import { Button, Modal, Form } from "react-bootstrap";
-//import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from './src/Header';
-//import { useAuth0 } from '@auth0/auth0-react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./src/Header";
 import LoginButton from "./src/Auth";
 import Profile from "./src/Profile";
-import HorizontalExample from "./src/Mediamakers"
-//const BACKEND_URL = import.meta.env.BACKEND_URL || "http://localhost:3001";
+import NewsSearch from "./src/NewsSearch";
+import HorizontalExample from "./src/Mediamakers";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Profile />
-        <HorizontalExample />
-        <LoginButton />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8">
+                <Routes>
+                  <Route path="/profile" component={Profile} />
+                  <Route path="/news" component={NewsSearch} />
+                  <Route path="/" exact component={Profile} />
+                </Routes>
+              </div>
+              <div className="col-md-4">
+                <LoginButton />
+              </div>
+            </div>
+          </div>
+          <HorizontalExample />
+        </div>
+      </Router>
     );
   }
 }
 
 export default App;
-
-
-
