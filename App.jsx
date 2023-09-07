0782eb877d15e0cbfe3a37d172571c127235f121
Ajamu Page <kil91402@gmail.com>
2023-09-06 19:41:01 -0500

    added navbar

---
+++ App.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+//import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+//import { Icon } from "@iconify/react";
+//import { Button, Modal, Form } from "react-bootstrap";
+//import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from './src/Header';
+//import { useAuth0 } from '@auth0/auth0-react';
+//import LoginButton from "./src/Auth";
+//import Profile from "./src/Profile";
+//const BACKEND_URL = import.meta.env.BACKEND_URL || "http://localhost:3001";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
