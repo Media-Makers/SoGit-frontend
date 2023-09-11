aa7af8e6581970311c39b451e70ab95dbad3becb
Andrea Malone <andreamurry63@gmail.com>
2023-09-11 17:06:08 -0500

    tookoutduplicatereactline4

--- src/NewsSearch.jsx
+++ src/NewsSearch.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Card } from "react-bootstrap";
-import React, { useState, useEffect } from "react";
+//import React, { useState, useEffect } from "react";
 const apiKey = import.meta.env.VITE_NEWS_API_KEY;
 
 const url = import.meta.env.VITE_BACKENDURL || "http://localhost:3001";
