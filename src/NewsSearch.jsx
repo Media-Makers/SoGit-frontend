9e449897098981c482897f6a1b1351a3bf9d1ca4
Justine Oyaghiro <justineoyaghiro@icloud.com>
2023-09-11 21:53:05 -0500

    Commit

--- src/NewsSearch.jsx
+++ src/NewsSearch.jsx
@@ -1,15 +1,17 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-
 import { useAuth0 } from "@auth0/auth0-react";
 import { Accordion, Card } from "react-bootstrap";
 import { Icon } from "@iconify/react";
-const apiKey = import.meta.env.VITE_NEWS_API_KEY;
+import "./news.css"
+
+// const apiKey = import.meta.env.VITE_NEWS_API_KEY;
 
 const url = import.meta.env.VITE_BACKENDURL || "http://localhost:3001";
 
 export default function NewsSearch() {
   const [articles, setArticles] = useState([]);
+
   useEffect(() => {
     const getNews = async () => {
       const newsData = await axios.get(`${url}/news`);
@@ -18,35 +20,35 @@ export default function NewsSearch() {
 
     getNews();
   }, [articles]);
-  const likeHandler = async (isLiked,id) => {
-    const result = await axios.patch(`${url}/likes/${id}`,{likes:isLiked});
-    console.log (result.data)
-    setArticles ([])
 
-  }
+  const likeHandler = async (isLiked, id) => {
+    const result = await axios.patch(`${url}/likes/${id}`, { likes: isLiked });
+    console.log(result.data);
+    setArticles([]);
+  };
 
   const { isAuthenticated } = useAuth0();
   return (
-    <>
+    <div className="feed-container">
       {articles.map((newsData, index) => (
-        <Card key={index}>
+        <div key={index}>
+        <Card >
           <Card.Body>
             <Card.Title>{newsData.title}</Card.Title>
             <Card.Subtitle>{newsData.desciption}</Card.Subtitle>
             <Card.Text>{newsData.content}</Card.Text>
             <div>
+            
               <Icon
                 icon={
                   newsData.likes ? "icon-park-twotone:like" : "icon-park:like"
                 }
-                onClick={()=>likeHandler(newsData.likes, newsData._id)}
-                
+                onClick={() => likeHandler(!newsData.likes, newsData._id)}
               />
-            
-             
             </div>
-            <Accordion defaultActiveKey="1">
-              <Accordion.Item eventKey="0">
+          
+            <Accordion>
+              <Accordion.Item >
                 <Accordion.Header>Comments</Accordion.Header>
                 <Accordion.Body>
                   {newsData.comments.map((comment, index) => (
@@ -55,9 +57,11 @@ export default function NewsSearch() {
                 </Accordion.Body>
               </Accordion.Item>
             </Accordion>
+          
           </Card.Body>
         </Card>
+        </div>
       ))}
-    </>
+    </div>
   );
 }
